b4f7357d4af116f89ad738341ccff1a636015bbf
DiptanilK <103043290+DiptanilK@users.noreply.github.com>
2022-04-06 02:21:53 -0700

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,409 @@
+from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')
+    from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')
+    from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')from ats.aetest import Testcase, setup, test, cleanup
+from ats import aetest
+
+import logging
+
+if __name__ == '__main__':
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Process parameters')v
